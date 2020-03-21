7bfe79359677c241a8d6258c1b5aefbcc0266fd9
malngaawy <mohager477@gmail.com>
2020-03-21 20:06:09 +0200

    section services

--- js/custom.js
+++ js/custom.js
@@ -4,6 +4,9 @@ $(function(){
   $(".navbar-nav li").each(function(){
 
     $(this).click(function(){
+      $(".navbar-toggler").addClass('collapsed').attr('aria-expanded', "false")
+
+      $(".navbar-collapse").removeClass('show');
 
       $(".intro").addClass("animated zoomOutDown");
 
