3701da9ac4cbff24ff0e5b543f40e401d3794961
malngaawy <mohager477@gmail.com>
2020-03-22 13:43:54 +0200

    Connect English

--- js/custom.js
+++ js/custom.js
@@ -14,10 +14,32 @@ $(function(){
       $(".intro").addClass("none");
 
       $($(this).attr('data-section')).addClass("animated zoomInDown").removeClass("none").siblings().addClass("none")
+      
+      if(!$(".connect").hasClass('none')) {
+        $("body").css({
+          "background-color":"#2b2728",
+          "border-color": "#f3f2ed",
+          })
+
+          $(".navbar-nav li a").each(function() {
+            $(this).addClass("use-connect")
+          })
+
+      } else {
+        $("body").css({
+          "background-color": "#f3f2ed",
+          "border-color": "#ffde16"
+        })
+
+        $(".navbar-nav li a").each(function() {
+          $(this).removeClass("use-connect")
+        })
+      }
 
     })
     
   })
+
 
 })
 
